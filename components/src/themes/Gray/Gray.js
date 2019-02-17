eafeadbf59a7a48a735aa9e2ea7a117077a1f25f
Shawn Nyquist <shawn.nyquist@gmail.com>
2019-02-17 15:15:29 -0600

    created and implemented the Gray theme

--- components/src/themes/Gray/Gray.js
+++ components/src/themes/Gray/Gray.js
@@ -38,11 +38,35 @@ const theme = {
 }
 
 const defaults = {
-    // Empty for now
+    button: {
+        padding: `${theme.space[2] / 16}em ${(theme.space[3] +4) / 16}em`,
+        border: theme.borders[3],
+        textTransform: 'uppercase',
+        letterSpacing: theme.letterSpacings.tracked
+    }
 }
 
 const variants = {
-    // Empty for now
+    button: {
+        primary: {
+            ...defaults.button,
+            color: theme.colors.primary.main,
+            borderColor: theme.colors.primary.main
+        },
+        contrast: {
+            ...defaults.button,
+            color: theme.colors.primary.contrastText,
+            borderColor: theme.colors.primary.contrastText
+        }
+    },
+    linkButton: {
+        primary: {
+            color: theme.colors.primary.main
+        },
+        contrast: {
+            color: theme.colors.primary.contrastText
+        }
+    },
 }
 
 const Gray = { ...theme, defaults, variants}
