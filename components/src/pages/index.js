b9f8c4bf6e03ab7c28811d3c35ee16b74fff6d85
Shawn Nyquist <shawn.nyquist@gmail.com>
2019-02-21 19:25:32 -0600

    index page updated

--- components/src/pages/index.js
+++ components/src/pages/index.js
@@ -1,14 +1,11 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from 'react'
+import { Link } from 'gatsby'
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
-
+import Layout from '../layouts/layout'
+import Image from '../components/image'
+import SEO from '../components/seo'
 import { Button, IconButton, Section } from '../components/Element'
 import { Search } from 'styled-icons/feather'
-
-import { Address, Button } from '../components/Element'
 
 const IndexPage = () => (
   <Layout>
@@ -17,7 +14,7 @@ const IndexPage = () => (
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />  
+      <Image />
     </div>
     <Section flex justifyContent="space-evenly" m={2}>
       <Link to="/page-2/">Go to page 2</Link>
